2b0e8ed56ab6ceb148ff3a6ffb3be8bab5e90baf
revzik <b.piekarz1997@gmail.com>
2020-04-12 16:24:53 +0200

    Add simple plot

---
+++ Lab2_31_03_2020/plot_1up_2down.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+
+# wyniki Bartka
+plt.plot([50, 42, 34, 26, 18, 10, 2, 10, 10, 2, 10, 10, 2, 7, 12, 12, 7, 7, 2, 7, 7, 2, 2, 5, 8, 8, 5, 5, 8, 8, 5, 5, 6, 7, 7, 6, 6, 5, 5, 6, 7, 7], marker='.')
+plt.grid()
+plt.show()
