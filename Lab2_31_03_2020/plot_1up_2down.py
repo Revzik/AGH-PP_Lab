6dcf714ea9aaee61d10d587903fb81de3f7be5d0
revzik <b.piekarz1997@gmail.com>
2020-04-21 00:43:58 +0200

    Add labels for axis

--- Lab2_31_03_2020/plot_1up_2down.py
+++ Lab2_31_03_2020/plot_1up_2down.py
@@ -2,12 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # wyniki Bartka
+# 2
 cents_bartek = [50, 42, 34, 26, 18, 10, 2, 10, 10, 18, 18, 10, 10, 2, 10, 10, 2, 6, 10, 10, 6, 10, 10, 6, 10, 10, 6, 10, 10, 6, 6, 10, 10, 6, 6, 2, 6, 6]
 turning_points_bartek = [2, 18, 2, 10, 2, 10, 6, 10, 6, 10, 6, 10, 6, 10, 2, 6]
 threshold_bartek = np.mean(turning_points_bartek[4:])
 
+plt.figure(1)
 plt.plot(cents_bartek, marker='.')
 plt.hlines(y=threshold_bartek, xmin=0, xmax=len(cents_bartek), linestyles='dashed')
 plt.title("IFC, adaptive method - Bartek\nThreshold: {:.2f} cents".format(threshold_bartek))
+plt.xlabel("trial number")
+plt.ylabel("frequency difference [cent]")
 plt.grid()
+
+
 plt.show()
--- Lab3_07-04-2020/plot_1up_2down.py
+++ Lab3_07-04-2020/plot_1up_2down.py
@@ -2,23 +2,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # wyniki Bartka
+# 1
 level_diff_bartek = np.abs([-6, -5, -4, -3, -2, -3, -3, -2, -2, -1, -2, -2, -1, -1.5, -1.5, -1.0, -1.5, -2.0, -2.0, -1.5, -1.5, -1.0, -1.0, -1.5, -1.5, -2.0, -2.0, -1.5, -1.5, -1.0, -1.5, -1.5, -2.0, -2.0, -1.5, -1.5, -2.0, -2.5, -2.5, -2.0, -2.0, -1.5, -2.0, -2.0])
 level_tp_bartek = np.abs([-2, -3, -1, -2, -1, -1.5, -1.0, -2.0, -1.0, -2.0, -1.0, -2.0, -1.5, -2.5, -1.5, -2.0])
 level_threshold_bartek = np.mean(level_tp_bartek[4:])
 
+plt.figure(1)
 plt.plot(level_diff_bartek, marker='.')
 plt.hlines(y=level_threshold_bartek, xmin=0, xmax=len(level_diff_bartek), linestyles='dashed')
 plt.title("Interaural level difference - Bartek\nThreshold: {:.1f}dB".format(level_threshold_bartek))
+plt.xlabel("trial number")
+plt.ylabel("volume difference [dB]")
 plt.grid()
-plt.show()
 
-
+# 2
 time_diff_bartek = [500, 450, 400, 350, 300, 250, 200, 150, 100, 50, 100, 150, 200, 200, 150, 150, 100, 100, 50, 100, 100, 150, 150, 200, 200, 150, 150, 175, 175, 150, 150, 125, 125, 100, 100, 75, 75, 50, 50, 25, 50, 75, 75, 50, 75, 100, 125, 125, 100, 100, 75, 100, 100, 75, 100, 100, 125, 150, 150, 125, 125, 100, 100, 125, 125, 150, 150]
 time_tp_bartek = [50, 200, 50, 200, 150, 175, 25, 75, 50, 125, 75, 100, 75, 150, 100, 150]
 time_threshold_bartek = np.mean(time_tp_bartek[4:])
 
+plt.figure(2)
 plt.plot(time_diff_bartek, marker='.')
 plt.hlines(y=time_threshold_bartek, xmin=0, xmax=len(time_diff_bartek), linestyles='dashed')
-plt.title("Interaural time difference - Bartek\nThreshold: {:.2f}us".format(time_threshold_bartek))
+plt.title("Interaural time difference - Bartek\nThreshold: {:.2f}$\mu$s".format(time_threshold_bartek))
+plt.xlabel("trial number")
+plt.ylabel("time difference [$\mu$s]")
 plt.grid()
+
+
 plt.show()
